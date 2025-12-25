969f9120884d6502f0b9f4a14d70f91a8348cbbf
Trung Anh <trunganh060107@gmail.com>
2025-12-25 14:10:52 +0700

    small fix

--- src/config.py
+++ src/config.py
@@ -4,7 +4,7 @@ from pathlib import Path
 # BASE_DIR points to the root of the titanic_project
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Data paths (Following your 'raw' data convention)
+# Data paths
 DATA_RAW_DIR = BASE_DIR / 'data'
 TRAIN_PATH = DATA_RAW_DIR / 'train.csv'
 TEST_PATH = DATA_RAW_DIR / 'test.csv'
